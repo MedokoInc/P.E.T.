adc45cd4a2761f3db511ec00b7a4da30b9e6794f
lisam <lisa.mokshantseva@gmail.com>
2022-02-21 11:09:50 +0100

    Created Composer

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require('vendor/autoload.php');
+
+use Medoko\Petlocator\Owner;
+
+$myOwner= new Owner('MOK', '+4300000000');
+echo $myOwner;
---
+++ src/Owner.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Medoko\Petlocator;
+
+class Owner
+{
+    private string $name;
+    private string $number;
+
+    /**
+     * @param string $name
+     * @param string $number
+     */
+    public function __construct(string $name, string $number)
+    {
+        $this->name = $name;
+        $this->number = $number;
+    }
+
+
+
+    public function __toString(){
+        return $this->getName(). ' (' .$this->getNumber(). ')';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+}
