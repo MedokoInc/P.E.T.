c75b12482144e2160ca1d2a898ac776059306df4
lisam <lisa.mokshantseva@gmail.com>
2022-02-28 10:55:50 +0100

    PhpUnit testing v.2 oopsie

---
+++ src/math.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Medoko\Petlocator;
+
+class math
+{
+    public static function add(int $x, int $y):int{
+        return $x +$y;
+    }
+}
---
+++ src/test.php
@@ -0,0 +1,6 @@
+<?php
+require_once('vendor/autoload.php');
+use Medoko\Petlocator\math ;
+
+echo math::add(3,2);
+
